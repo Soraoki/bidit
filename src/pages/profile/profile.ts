13b55795511ef7f7dc5d8099bb2d3532e7d50dec
Soraoki <themusiquephille@gmail.com>
2018-03-23 20:17:06 +0530

    v0.1

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, App } from 'ionic-angular';
 
 /**
  * Generated class for the ProfilePage page.
@@ -15,11 +15,15 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class ProfilePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public app: App) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ProfilePage');
   }
 
+  logoutf() {
+    const root = this.app.getRootNav();
+    root.popToRoot();
+  }
 }
